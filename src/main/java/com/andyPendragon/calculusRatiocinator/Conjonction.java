a33744e0491040a5d1814a560972b6eeb82ba992
AndyPendragon <116746292+AndyPendragon@users.noreply.github.com>
2024-06-07 16:32:37 +0300

    feat: conjonction::operations

--- src/main/java/com/andyPendragon/calculusRatiocinator/Conjonction.java
+++ src/main/java/com/andyPendragon/calculusRatiocinator/Conjonction.java
@@ -4,4 +4,25 @@ public enum Conjonction {
     et,
     ou,
     donc;
+
+    public static Affirmation et(Affirmation affirmation1, Affirmation affirmation2){
+        if ((affirmation1 == Affirmation.vrai) && affirmation2 == Affirmation.vrai){
+            return Affirmation.vrai;
+        }
+        else return Affirmation.faux;
+    }
+
+    public static Affirmation ou(Affirmation affirmation1, Affirmation affirmation2){
+        if ((affirmation1 == Affirmation.vrai) || affirmation2 == Affirmation.vrai) {
+            return Affirmation.vrai;
+        }
+        else return Affirmation.faux;
+    }
+
+    public static Affirmation donc(Affirmation affirmation1, Affirmation affirmation2){
+        if ((affirmation1 == Affirmation.vrai) && affirmation2 == Affirmation.faux) {
+            return Affirmation.faux;
+        }
+        else return Affirmation.vrai;
+        }
 }
